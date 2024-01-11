32045db902f10c0c16d0f376df059b69c988d33f
rashmip <rashmi.phadnis98@gmail.com>
2024-01-10 21:41:55 -0500

    Update run_expt.py

--- run_expt.py
+++ run_expt.py
@@ -3,6 +3,8 @@ import torch
 from model import IdiographicClassifier
 from dataset import BuildDataset, BuildDataloader
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main():
@@ -10,22 +12,16 @@ def main():
         params_loaded = yaml.safe_load(stream)
 
     data_csv = pd.read_csv(params_loaded['data']['csv_path'])
-    unique_ids = data_csv['userID'].unique().tolist()
-    
-    # img_nms = data_csv['stimulus'].tolist()
-    # img_paths = []
-    # for img in img_nms:
-    #     img_paths.append(params_loaded['data']['imgs_path']+'/'+img[36:44])
+    unique_ids = data_csv['userID'].unique().tolist()    
     
     if params_loaded['test']==False:
         pretrained_model = torch.hub.load('pytorch/vision:v0.10.0', params_loaded['model']['pretrained'], pretrained=True)
         feature_extractor = torch.nn.Sequential(*list(pretrained_model.children())[:-1*params_loaded['model']['layer']])
         feature_extractor.eval()
         model = IdiographicClassifier(params_loaded, pretrained_model.fc.in_features)
-        optimizer = torch.optim.Adam(model.parameters(), lr=params_loaded['train']['lr'], momentum=params_loaded['train']['momentum'])
-        best_vloss = 1000000.
+        optimizer = torch.optim.Adam(model.parameters(), lr=params_loaded['train']['lr'], betas=(params_loaded['train']['betas'][0],params_loaded['train']['betas'][1]))
    
-        if params_loaded['individual']:
+        if params_loaded['model']['individual']:
             for user in unique_ids:
                 df = data_csv.loc[data_csv['userID'] == user]
 
@@ -37,12 +33,9 @@ def main():
                     torch.save(model.state_dict(), params_loaded['save']+'/'+model_path)
         
         else:
-            avg_train_loss, avg_val_loss = train_loop(model, feature_extractor, optimizer, data_csv, params_loaded)
+            train_loop(model, feature_extractor, optimizer, data_csv, params_loaded)
             
-            if avg_val_loss < best_vloss:
-                best_vloss = avg_val_loss
-                model_path = 'global'
-                torch.save(model.state_dict(), params_loaded['save']+'/'+model_path)
+            torch.save(model.state_dict(), params_loaded['save']+'/'+'global.pth')
 
 
 def train_loop(model, feature_extractor, optimizer, df, params_loaded):
@@ -52,17 +45,20 @@ def train_loop(model, feature_extractor, optimizer, df, params_loaded):
     test_size = int((full_size - train_size)/2)
     val_size = full_size - train_size - test_size
     
-    trainset, valset = torch.utils.data.random_split(dataset, [train_size, val_size])
+    trainset, valset, testset = torch.utils.data.random_split(dataset, [train_size, val_size, test_size])
 
     train_build_loader = BuildDataloader(trainset, batch_size=params_loaded['train']['batch_size'], shuffle=True, num_workers=4)
     train_loader = train_build_loader.loader()
 
     val_build_loader = BuildDataloader(valset, batch_size=params_loaded['train']['batch_size'], shuffle=True, num_workers=4)
     val_loader = val_build_loader.loader()
-
-    for epoch in params_loaded['train']['epochs']:
+    train_loss=  []
+    val_loss = []
+    for epoch in range(params_loaded['train']['epochs']):
         model.train()
+        train_epoch_loss = []
         for i,batch in enumerate(train_loader,0):
+            
             imgs = batch['images']
             ratings = batch['ratings']
 
@@ -73,21 +69,18 @@ def train_loop(model, feature_extractor, optimizer, df, params_loaded):
             outputs = model(feats)
 
             # Compute the loss and its gradients
-            loss = model.compute_loss(outputs, ratings)
+            loss = model.compute_loss(torch.squeeze(outputs), ratings)
             loss.backward()
 
             # Adjust learning weights
             optimizer.step()
 
             # Gather data and report
-            running_train_loss += loss.item()
-            if i % 1000 == 999:
-                last_loss = running_loss / 1000 # loss per batch
-                print('  batch {} loss: {}'.format(i + 1, last_loss))
-                running_loss = 0.
+            train_epoch_loss.append(loss.item())
+        train_loss.append(sum(train_epoch_loss)/len(train_epoch_loss))
 
         model.eval()
-        running_vloss = 0.
+        val_epoch_loss = []
         with torch.no_grad():
             for i, batch in enumerate(val_loader):
                 imgs = batch['images']
@@ -95,10 +88,15 @@ def train_loop(model, feature_extractor, optimizer, df, params_loaded):
                 feats = feature_extractor(imgs)
                 out = model(feats)
                 vloss = model.compute_loss(out, ratings)
-                running_vloss +=vloss
-        avg_vloss = running_vloss / (i+1)
+                val_epoch_loss.append(vloss)
+        val_loss.append(sum(val_epoch_loss)/len(val_epoch_loss))
 
-        #return last_loss, avg_vloss
+    plt.plot(np.linspace(1,params_loaded['train']['epochs'],params_loaded['train']['epochs']).astype(int), train_loss, label='train')
+    plt.plot(np.linspace(1,params_loaded['train']['epochs'],params_loaded['train']['epochs']).astype(int), val_loss, label='val')  
+    plt.legend()
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")  
+    plt.savefig(params_loaded['save']+"loss.jpg")
 
 if __name__ == '__main__':
     main()
